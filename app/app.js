b918c3e9b6ec355639184b34f6976e003de69b8e
lxl <rembern@126.com>
2019-03-03 22:15:20 +0800

    init

---
+++ app/app.js
@@ -0,0 +1,45 @@
+/**
+ * Created by lxl on 2019/3/3.
+ */
+var app = angular.module("myApp",[]);
+app.controller("myCtrl", function ($scope) {
+    //初始化一些数据
+    $scope.datas = ["key4","xyz","key3","xxxx","key2","value2","key1","value1"];  //下拉框中的数据
+    $scope.copyDatas = $scope.datas;   //下拉框中的数值拷贝一份
+    $scope.hide = true; //显示隐藏下拉框
+    $scope.textValue = "";  //文本框数值
+
+    //将下拉选的数据值赋值给文本框，并且隐藏下拉框
+    $scope.change = function (x) {
+        $scope.hide = true;  //下拉框隐藏
+        $scope.textValue = x;   //文本框中的值
+
+    };
+    //获取的数据值与下拉选逐个比较，如果包含则放在临时变量副本，并用临时变量副本替换下拉选原先的数值，如果数据为空或找不到，就用初始下拉选项副本替换
+    $scope.changeKeyValue = function (v) {
+        var newData = [];  //创建一个临时下拉框副本
+        angular.forEach($scope.datas, function (data, index, array) {
+            //如果
+            if (data.indexOf(v)>=0){
+                newData.unshift(data);
+            }
+        });
+        $scope.datas = newData; //newData中的数值赋值给$scope.datas
+        $scope.hide = false;   //文本框显示
+
+        //如果不包含或者输入框中的值为空时，把拷贝出的$scope.copyDatas赋值给$scope.datas
+        if ($scope.datas.length==0||v==""){
+            $scope.datas = $scope.copyDatas;
+        }
+    };
+
+    //输入框中的值必须是下拉选择框中的数值
+    $scope.test = function () {
+        if($scope.datas.length>1){
+            $scope.hide = false;
+        }
+    };
+
+    console.log($scope.datas.length);
+})
+
